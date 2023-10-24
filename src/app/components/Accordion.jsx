50a04128241a6307b7dd6150c2131b1637bb6bc3
Ashik Jhonson <ashikjhonson@gmail.com>
2023-10-25 04:18:10 +0530

    Accordion complete

--- src/app/components/Accordion.jsx
+++ src/app/components/Accordion.jsx
@@ -17,38 +17,21 @@ export default function AccordionCustomIcon({ items }) {
             let name = item.split(".")[0].replace("_", " ");
             console.log(name);
             return (
-              <div>
-                <Accordion allowToggle>
-                  <AccordionItem>
-                    <h2>
-                      <AccordionButton>
-                        <Box as="span" flex="1" textAlign="left">
-                          <p className="capitalize">{name}</p>
-                        </Box>
-                        <AccordionIcon />
-                      </AccordionButton>
-                    </h2>
-                    <AccordionPanel pb={4}>
-                      <File title={item} code={value} />
-                    </AccordionPanel>
-                  </AccordionItem>
-                </Accordion>
-
-                {/* <Accordion
-                  open={open === name}
-                  icon={<Icon id={name} open={name} />}
-                  key={name}
-                >
-                  <AccordionHeader onClick={() => handleOpen(name)}>
-                    <p className="capitalize">{name}</p>
-                  </AccordionHeader>
-                  <AccordionBody>
-                    <div className="overflow-hidden">
-                      <File title={name} code={value} />
-                    </div>
-                  </AccordionBody>
-                </Accordion> */}
-              </div>
+              <Accordion allowToggle key={item}>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <Box as="span" flex="1" textAlign="left">
+                        <p className="capitalize">{name}</p>
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>
+                    <File title={item} code={value} />
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
             );
           })
         : null}
